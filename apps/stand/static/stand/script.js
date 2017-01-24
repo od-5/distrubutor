8463ecfee76e22b5c9ab2c023a4ea509b8e8fe19
alexy <od-5@yandex.ru>
2017-01-24 20:51:17 +0300

    test stand

--- apps/stand/static/stand/script.js
+++ apps/stand/static/stand/script.js
@@ -2,60 +2,58 @@
  * Created by alexy on 23.01.17.
  */
 $(function() {
-  //$('#standContainer')
-  //  .bind("mousedown", function(e) {e.metaKey = true;})
-  //  .selectable();
   $('#standContainer')
 	    .bind("mousedown", function(e) {e.metaKey = false;})
 	    .selectable();
   var minBlockHeight = 120;
   var minBlockWidth = 200;
-  function sortingCoords (a, b) {
-      var az = Math.sqrt(a[0] * a[0] + a[1] * a[1]);
-      var bz = Math.sqrt(b[0] * b[0] + b[1] * b[1]);
-      if(az == bz) return 0;
-      return (az < bz) ? -1 : 1;
-  }
+
   $('#js-combine-units').click(function(){
     var selected = $('.ui-selected');
-    console.log(selected.length);
     if (selected.length > 1) {
-      var error_flag = 0;
+      var x_coords = [];
+      var y_coords = [];
+      var x1_coords = [];
+      var y1_coords = [];
       var top1 = selected.first().position()['top'];
       var left1 = selected.first().position()['left'];
       var top2 = selected.last().position()['top'];
       var left2 = selected.last().position()['left'];
       var blockWidth = selected.last().outerWidth();
       var blockHeight = selected.last().outerHeight();
-      var width = left2 - left1 + blockWidth;
-      var height = top2 - top1 + blockHeight;
-      var coords = [];
-      //selected.each(function(){
-      //  coords.push([$(this).position()['left'], $(this).position()['top']]);
-      //});
-      //console.log(coords);
-      //console.log(coords.sort(sortingCoords));
-      if (width < 200){
-        console.log('так нельзя выбирать!');
-        error_flag = 1;
-      }
-      if (height < 120){
-        error_flag = 1;
-      }
-      console.log('top1='+top1+'left1='+left1+'top2='+top2+'left2='+left2+'width='+width+'height='+height);
-      console.log(selected.last());
-      if (error_flag == 0) {
-        //вставить перед выделенными блоками новый
+      var new_width = left2 - left1 + blockWidth;
+      var new_height = top2 - top1 + blockHeight;
+      var min_x, max_x, min_y, max_y;
+      var s = 0, s1;
+      selected.each(function(){
+        left = $(this).position()['left'];
+        top = $(this).position()['top'];
+        width = $(this).outerWidth();
+        height = $(this).outerHeight();
+        y_coords.push($(this).position()['top']);
+        y1_coords.push($(this).position()['top']+height);
+        x_coords.push($(this).position()['left']);
+        x1_coords.push($(this).position()['left']+width);
+        s += width*height;
+      });
+      min_x = Math.min.apply(null, x_coords);
+      max_x = Math.max.apply(null, x1_coords);
+      min_y = Math.min.apply(null, y_coords);
+      max_y = Math.max.apply(null, y1_coords);
+      s1 = (max_y - min_y) * (max_x - min_x);
+
+      if (s==s1) {
         selected.first().before(
           '<div class="block" style="top:'+ top1 +
           'px;left:' + left1 +
-          'px;width:' + width +
-          'px;height:' + height +
+          'px;width:' + new_width +
+          'px;height:' + new_height +
           'px;z-index:10;">' +
           'Новый блок</div>'
         );
-        //удалить выделенные блоки
         selected.remove();
+      } else {
+        alert('Нельзя объединить!');
       }
     } else {
       alert('Нужно выбрать больше одно элемента');
