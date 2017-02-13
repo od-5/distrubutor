71d1c49300a6cdd2e6e6580284ddc1970d1a886f
Fukalov Dmitri <2mitrij@gmail.com>
2017-02-13 18:01:09 +0300

    Partially refactored forum app.

--- apps/forum/views.py
+++ apps/forum/views.py
@@ -1,16 +1,13 @@
 # coding=utf-8
-from annoying.functions import get_object_or_None
-from django.conf import settings
-from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import ListView
-from apps.administrator.decorators import administrator_required
+from django.views.generic import ListView, CreateView, UpdateView
+
+from lib.cbv import SendUserToFormMixin
 from .models import Section, Topic, Comment, Notification
 from .forms import SectionAddForm, TopicAddForm, CommentAddForm, SectionUpdateForm, TopicUpdateForm, CommentUpdateForm
-from core.models import User
 
 __author__ = 'alexy'
 
@@ -20,105 +17,95 @@ class SectionListView(ListView):
     template_name = 'forum/section_list.html'
 
 
-@administrator_required
-def section_add(request):
-    user = request.user
-    if request.method == 'POST':
-        form = SectionAddForm(request.POST, user=user)
-        if form.is_valid():
-            form.save()
+class SectionCreateView(CreateView, SendUserToFormMixin):
+    form_class = SectionAddForm
+    template_name = 'forum/section_add.html'
+    success_url = reverse_lazy('forum:list')
+
+
+# TODO: не используется
+class SectionUpdateView(UpdateView):
+    model = Section
+    form_class = SectionUpdateForm
+    template_name = 'forum/section_update.html'
+    success_url = reverse_lazy('forum:list')
+
+
+class TopicListView(ListView):
+    template_name = 'forum/topic_list.html'
+
+    def get_queryset(self):
+        user = self.request.user
+        section = Section.objects.get(pk=self.kwargs['pk'])
+        qs = section.topic_set.all()
+        if user.type == 2:
+            qs = qs.filter(moderator=True)
+            city_list = [city.id for city in user.moderator_user.city.all()]
+            qs = qs.filter(city__in=city_list) | qs.filter(all_city=True)
+        elif user.type == 5:
+            if user.manager_user.leader:
+                qs = qs.filter(leader=True)
+            else:
+                qs = qs.filter(manager=True)
+            city_list = [city.id for city in user.manager_user.moderator.moderator_user.city.all()]
+            qs = qs.filter(city__in=city_list) | qs.filter(all_city=True)
+        elif user.type == 4:
+            qs = qs.filter(distributor=True)
+            qs = qs.filter(city=user.adjuster.city.id) | qs.filter(all_city=True)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(TopicListView, self).get_context_data(**kwargs)
+        context.update({
+            'object': Section.objects.get(pk=self.kwargs['pk']),
+        })
+        return context
+
+
+class TopicCreateView(CreateView, SendUserToFormMixin):
+    model = Topic
+    form_class = TopicAddForm
+    template_name = 'forum/topic_add.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            self.section = Section.objects.get(pk=request.GET.get('section'))
+        except Section.DoesNotExist:
             return HttpResponseRedirect(reverse('forum:list'))
-    else:
-        form = SectionAddForm(user=user)
-    context = {
-        'form': form
-    }
-    return render(request, 'forum/section_add.html', context)
+        return super(TopicCreateView, self).dispatch(request, *args, **kwargs)
+
+    def get_initial(self):
+        return {'section': self.section}
+
+    def get_success_url(self):
+        return reverse('forum:topic-list', args=(self.section.pk, ))
+
+    def form_valid(self, form):
+        result = super(TopicCreateView, self).form_valid(form)
+        self.object.notification_recipients()
+        return result
+
+    def get_context_data(self, **kwargs):
+        context = super(TopicCreateView, self).get_context_data(**kwargs)
+        context['section'] = self.section
+        return context
 
 
-@administrator_required
-def section_update(request, pk):
-    section = Section.objects.get(pk=int(pk))
-    if request.method == 'POST':
-        form = SectionUpdateForm(request.POST, instance=section)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('forum:list'))
-    else:
-        form = SectionUpdateForm(instance=section)
-    context = {
-        'form': form
-    }
-    return render(request, 'forum/section_update.html', context)
+class TopicUpdateView(UpdateView):
+    model = Topic
+    form_class = TopicUpdateForm
+    template_name = 'forum/topic_update.html'
+
+    def get_success_url(self):
+        return reverse('forum:topic-detail', args=(self.object.pk, ))
+
+    def get_context_data(self, **kwargs):
+        context = super(TopicUpdateView, self).get_context_data(**kwargs)
+        context['section'] = self.object.section
+        return context
 
 
-@login_required
-def topic_list(request, pk):
-    user = request.user
-    context = {}
-    section = Section.objects.get(pk=int(pk))
-    qs = section.topic_set.all()
-    if user.type == 2:
-        qs = qs.filter(moderator=True)
-        city_list = [city.id for city in user.moderator_user.city.all()]
-        qs = qs.filter(city__in=city_list) | qs.filter(all_city=True)
-    elif user.type == 5:
-        if user.manager_user.leader:
-            qs = qs.filter(leader=True)
-        else:
-            qs = qs.filter(manager=True)
-        city_list = [city.id for city in user.manager_user.moderator.moderator_user.city.all()]
-        qs = qs.filter(city__in=city_list) | qs.filter(all_city=True)
-    elif user.type == 4:
-        qs = qs.filter(distributor=True)
-        qs = qs.filter(city=user.adjuster.city.id) | qs.filter(all_city=True)
-    context.update({
-        'object': section,
-        'object_list': qs
-    })
-    return render(request, 'forum/topic_list.html', context)
-
-
-@login_required
-def topic_add(request):
-    user = request.user
-    try:
-        section = Section.objects.get(pk=int(request.GET.get('section')))
-    except:
-        return HttpResponseRedirect(reverse('forum:list'))
-    if request.method == 'POST':
-        form = TopicAddForm(request.POST, user=user)
-        if form.is_valid():
-            topic = form.save()
-            topic.notification_recipients()
-            return HttpResponseRedirect(reverse('forum:topic-list', args=(section.id, )))
-    else:
-        form = TopicAddForm(user=user, initial={'section': section})
-    context = {
-        'form': form,
-        'section': section
-    }
-    return render(request, 'forum/topic_add.html', context)
-
-
-@login_required
-def topic_update(request, pk):
-    topic = Topic.objects.get(pk=int(pk))
-    if request.method == 'POST':
-        form = TopicUpdateForm(request.POST, instance=topic)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('forum:topic-detail', args=(topic.id, )))
-    else:
-        form = TopicUpdateForm(instance=topic)
-    context = {
-        'form': form,
-        'object': topic,
-        'section': topic.section
-    }
-    return render(request, 'forum/topic_update.html', context)
-
-
+# TODO: продумать красивое решение на CBV, переписать
 @login_required
 def topic_detail(request, pk):
     user = request.user
@@ -142,35 +129,23 @@ def topic_detail(request, pk):
     return render(request, 'forum/topic_detail.html', context)
 
 
-@login_required
-def topic_notify(request):
-    user = request.user
-    qs = Notification.objects.filter(user=user)
-    context = {
-        'object_list': qs
-    }
-    return render(request, 'forum/topic_notify.html', context)
+class TopicNotifyListView(ListView):
+    template_name = 'forum/topic_notify.html'
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
 
 
-@login_required
-def comment_update(request, pk):
-    context = {}
-    comment = Comment.objects.get(pk=int(pk))
-    topic = comment.topic
-    if request.method == 'POST':
-        form = CommentUpdateForm(request.POST, instance=comment)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('forum:topic-detail', args=(topic.id, )))
-    else:
-        form = CommentUpdateForm(instance=comment)
-    context.update({
-        'object': comment,
-        'form': form
-    })
-    return render(request, 'forum/comment_update.html', context)
+class CommentUpdateView(UpdateView):
+    model = Comment
+    form_class = CommentUpdateForm
+    template_name = 'forum/comment_update.html'
+
+    def get_success_url(self):
+        return reverse('forum:topic-detail', args=(self.object.topic.pk, ))
 
 
+# TODO: переделать логику под CBV и переписать
 @login_required
 def comment_delete(request, pk):
     context = {}
@@ -187,6 +162,7 @@ def comment_delete(request, pk):
     return render(request, 'forum/comment_delete.html', context)
 
 
+# TODO: переделать логику под CBV и переписать
 @login_required
 def topic_delete(request, pk):
     context = {}
@@ -203,6 +179,7 @@ def topic_delete(request, pk):
     return render(request, 'forum/topic_delete.html', context)
 
 
+# TODO: переделать логику под CBV и переписать
 @login_required
 def topic_close(request, pk):
     user = request.user
