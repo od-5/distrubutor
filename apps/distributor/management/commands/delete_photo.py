97f5e0b0a1c855b428ec7e998243ee342ace61da
alexy <od-5@yandex.ru>
2017-03-14 10:34:29 +0300

    add delete photo command

---
+++ apps/distributor/management/commands/delete_photo.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+from django.core.management.base import BaseCommand, CommandError
+import datetime
+from apps.distributor.models import PointPhoto
+
+__author__ = 'alexy'
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        search_date = datetime.datetime.now() - datetime.timedelta(days=60)
+        qs = PointPhoto.objects.filter(timestamp__lt=search_date)
+        print qs.count()
+        if qs:
+            qs.delete()
