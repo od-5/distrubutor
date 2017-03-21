187f0956ad5ae83c983a8e3ef0045b11209f2b28
alexy <od-5@yandex.ru>
2017-03-21 12:05:34 +0300

    fix landing urls

--- landing/urls.py
+++ landing/urls.py
@@ -10,7 +10,7 @@ __author__ = 'alexy'
 urlpatterns = patterns(
     'landing.views',
     url(r'^$', LandingView.as_view(), name='index'),
-    url(r'^(?P<slug>[\w-]+)$', CityDetailView.as_view(), name='city'),
+    url(r'^(?P<slug>[\w-]+)/$', CityDetailView.as_view(), name='city'),
     url(r'^thnx/$', TemplateView.as_view(template_name='landing/ok.html'), name='thnx'),
     url(r'^set_current_city_from_input/$', 'set_current_city_from_input', name='set_current_city_from_input'),
 )
